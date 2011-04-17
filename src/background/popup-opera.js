d6507ad312d3b3fa7b0cc289d168ac0c8af1cf69
Atsushit Takayama <taka.atsushi@gmail.com>
2011-04-17 08:04:22 -0400

    ログイン・ログアウトを実装

--- src/background/popup-opera.js
+++ src/background/popup-opera.js
@@ -35,5 +35,54 @@ if (typeof opera === 'object') {
     document.addEventListener('DOMContentLoaded', function() {
         // don't know why this element is here
         $('#comment-show').hide();
+
+        // add logout button
+        $('#username').parent()
+        .append('<input type="button" value="ログアウト">')
+        .click(function() {
+            this.disabled = true;
+            $.get('http://www.hatena.ne.jp/logout').next(function() {
+                opera.extension.postMessage({message: 'logout'});
+                setTimeout(function() {
+                    ViewManager.show('bookmark');
+                }, 500);
+            });
+        });
+
+        // add login form
+        $('a[href^="http://www.hatena.ne.jp/login"]').click(function() {
+            if ($('#loginform').length) return false;
+            $([
+                '<form id="loginform">',
+                '<p>ユーザー名 <input id="login_username" /></p>',
+                '<p>パスワード <input type="password" id="login_password" /></p>',
+                '<p><input type="submit" value="ログイン" /></p>',
+                '</form>'
+            ].join('\n'))
+            .appendTo($(this).parent())
+            .submit(function() {
+                $(this).find('input').attr('disabled', 'disabled');
+                var name = $(this).find('#login_username').val();
+                var pass = $(this).find('#login_password').val();
+                if (!name) {
+                    $('#bookmark-login-header').text('ユーザー名が空欄です');
+                } else if (!pass) {
+                    $('#bookmark-login-header').text('パスワードが空欄です');
+                } else {
+                    $.post('http://www.hatena.ne.jp/login', {
+                        name: name,
+                        password: pass
+                    }).next(function() {
+                        opera.extension.postMessage({message: 'login_check'});
+                        $('#bookmark-login-header').text('ログイン完了。3秒後にリロードします。切り替わらない場合は再度開いてください。');
+                        setTimeout(function() {
+                            location.reload();
+                        }, 3000);
+                    });
+                }
+                return false;
+            });
+            return false;
+        });
     }, false);
 }
