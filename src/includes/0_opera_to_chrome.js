3c83354cd055d46d0b0a11ad73aa94c4678b163c
Atsushit Takayama <taka.atsushi@gmail.com>
2011-04-17 08:15:29 -0400

    InjectedScript でのエイリアス追加 Node = window.Node

--- src/includes/0_opera_to_chrome.js
+++ src/includes/0_opera_to_chrome.js
@@ -3,6 +3,7 @@ if (typeof opera === 'object') {
     location = window.location;
     HTMLImageElement = window.HTMLImageElement;
     XPathResult = window.XPathResult;
+    Node = window.Node;
     navigator = window.navigator;
 
     // chrome util
